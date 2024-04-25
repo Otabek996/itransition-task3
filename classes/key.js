6aefc8c6c04b0c542db0775c527091026a7edabb
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-04-25 15:10:36 +0500

    feat: create key class

--- classes/key.js
+++ classes/key.js
@@ -1,1 +1,19 @@
 // Key
+import { randomBytes, createHmac } from "node:crypto";
+
+class Key {
+  constructor(move) {
+    this.move = move;
+  }
+
+  update() {
+    const key = randomBytes(32).toString("hex");
+    const compMove = Math.round(Math.random() * this.move.length);
+    const hmac = createHmac("sha3-256", key)
+      .update(this.move[compMove])
+      .digest("hex");
+    return { key, compMove, hmac };
+  }
+}
+
+export default Key;
