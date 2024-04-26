0f38a3c852db83867d1e331c11ee480e314d8d87
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-04-26 11:55:58 +0500

    feat: create table class

--- classes/table.js
+++ classes/table.js
@@ -1,1 +1,24 @@
 // Table
+import tableCreator from "cli-table3";
+
+class Table {
+  constructor(actions, result) {
+    this.actions = actions;
+    this.result = result;
+  }
+
+  render() {
+    const table = new tableCreator({
+      style: { head: [], border: [] },
+      head: ["v PC \\ User >", ...this.actions],
+    });
+
+    for (let i = 0; i < this.actions.length; i++) {
+      table.push({ [this.actions[i]]: this.result[i] });
+    }
+
+    console.log(table.toString());
+  }
+}
+
+export default Table;
