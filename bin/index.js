929449bc7cb59f275866ae324c725b2a296dcb53
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-04-26 16:05:30 +0500

    fix: fix bug

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,16 @@
 // Index
 import { argv } from "node:process";
 import App from "../app.js";
+import { validateArgs } from "../helpers/helpers.js";
 
 const main = () => {
   const actions = argv.slice(2);
+  console.log(actions);
 
-  const app = new App(actions);
-  app.loading();
+  if (validateArgs(actions)) {
+    const app = new App(actions);
+    app.loading();
+  }
 };
 
 main();
