478e2c6ca5533ed95fcd673540bb84767afa9c0d
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-04-26 16:16:13 +0500

    fix: fix bug

--- options/playAgain.js
+++ options/playAgain.js
@@ -5,7 +5,7 @@ const playAgain = async (again = false) => {
   const answer = await inquirer.prompt({
     name: "play",
     type: "confirm",
-    message: `Do you want to play ${again ? "again" : ""}? (y/n)`,
+    message: `Do you want to play ${again ? "again" : ""}?`,
   });
 
   return answer.play;
