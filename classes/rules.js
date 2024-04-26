8718ee874a958f1698766b26330c1b0ff4527f70
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-04-26 12:14:35 +0500

    feat: create rules feat

--- classes/rules.js
+++ classes/rules.js
@@ -5,7 +5,28 @@ class Rules {
     this.actions = actions;
   }
 
-  results() {}
+  results() {
+    const tableLength = this.actions.length;
+    const tableArray = Array.from({ length: tableLength }, () => []);
+    const tableIndex = Math.floor(tableLength / 2);
+    let index;
+
+    for (let i = 0; i < tableLength; i++) {
+      for (let j = 0; j < tableLength; j++) {
+        index = Math.sign(((j - i + tableIndex + tableLength) % tableLength) - tableIndex);
+
+        if (index > 0) {
+          tableArray[i].push("Win");
+        } else if (index < 0) {
+          tableArray[i].push("Lose");
+        } else {
+          tableArray[i].push("Draw");
+        }
+      }
+    }
+
+    return tableArray;
+  }
 }
 
 export default Rules;
